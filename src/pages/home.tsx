a24c84adc9c4cd2dfbec34edbc8bfdfa46f02a4a
TravisWongX <travis.x.wong@gmail.com>
2022-02-25 11:02:10 +0800

    完成Chart3折线图

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,18 +3,19 @@ import './home.scss'
 import headerBg from '../images/header.png'
 import { Chart1 } from '../components/Chart_1'
 import { Chart2 } from '../components/Chart_2'
+import { Chart3 } from '../components/Chart_3'
 
 export const Home = () => {
   const year = new Date().getFullYear()
   const [now, setNow] = useState(new Date().toLocaleString())
 
   useEffect(() => {
-    setInterval(()=> setNow(new Date().toLocaleString()), 1000)
-  },[])
+    setInterval(() => setNow(new Date().toLocaleString()), 1000)
+  }, [])
 
   return (
     <div className="home">
-      <header style={{ backgroundImage: `url(${headerBg})` }} >
+      <header style={{ backgroundImage: `url(${headerBg})` }}>
         <h1>西虹市公安局合成作战平台</h1>
         <span>{now}</span>
       </header>
@@ -23,15 +24,14 @@ export const Home = () => {
           <Chart1 />
           <Chart2 />
         </section>
-        <section className="section2 bordered"></section>
+        <section className="section2">
+          <Chart3 />
+        </section>
         <section className="section3 bordered"></section>
         <section className="section4 bordered"></section>
         <section className="section5 bordered"></section>
       </main>
-      <footer>
-      &copy; 西虹市公安局大数据中心 2020-{year}
-      </footer>
-
+      <footer>&copy; 西虹市公安局大数据中心 2020-{year}</footer>
     </div>
   )
 }
