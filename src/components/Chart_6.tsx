bf49187a4813c3a4c125cb453528bf9c1771603c
TravisWongX <travis.x.wong@gmail.com>
2022-02-25 21:54:57 +0800

    显示全市地图

--- src/components/Chart_6.tsx
+++ src/components/Chart_6.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import china from '../geo/china.json'
+import React, { useRef, useEffect } from 'react'
+import province from '../geo/geoData.json'
+import * as echarts from 'echarts'
+import { createEchartsOptions } from '../shared/create-echart-options'
+import { px } from '../shared/px'
 
 export const Chart6 = () => {
+  const divRef = useRef(null)
+  useEffect(() => {
+    const myChart = echarts.init(divRef.current)
+    // @ts-ignore
+    echarts.registerMap('GY', province)
+    myChart.setOption(
+      createEchartsOptions({
+        xAxis: { show: false },
+        yAxis: { show: false },
+        grind: {
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 0,
+        },
+        series: [
+          {
+            type: 'map',
+            mapType: 'GY', // 自定义扩展图表类型
+            // data: [{ name: '云南省', value: 0 }],
+            label: { show: false, color: 'white' },
+            itemStyle: {
+              areaColor: '#010D3D',
+              borderColor: '#01A7F7',
+              emphasis: {
+                label: { color: 'white' },
+                areaColor: '#5470C6',
+              },
+            },
+          },
+        ],
+      })
+    )
+  }, [])
+
   return (
     <div className="bordered 籍贯">
-      <h2>全市犯罪人员籍贯分布</h2>
+      <h2>全市案情侦破实时数据</h2>
+      <div ref={divRef} className="chart" />
     </div>
   )
 }
