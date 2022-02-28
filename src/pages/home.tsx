809226d4d7cae08003fbc58b9e0c926f0a88d9d5
TravisWongX <travis.x.wong@gmail.com>
2022-02-28 16:53:34 +0800

    布局Chart10/11/12

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -48,7 +48,19 @@ export const Home = () => {
             </div>
           </div>
         </section>
-        <section className="section5 bordered"></section>
+        <section className="section5">
+          <div className="bordered row1 案发类型">
+            <h2>案发类型统计</h2>
+            <div className="charts"></div>
+          </div>
+          <div className="bordered row2 案发街道">
+            <h2>案发街道统计</h2>
+            <div className="charts"></div>
+          </div>
+          <div className="bordered row3 作案手段">
+            <h2>作案手段分析</h2>
+          </div>
+        </section>
       </main>
       <footer>&copy; 西虹市公安局大数据中心 2020-{year}</footer>
     </div>
