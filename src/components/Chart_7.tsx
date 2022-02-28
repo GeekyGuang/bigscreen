0b92c1fa8bec9c18f80f487f7feedaad09ee78a8
TravisWongX <travis.x.wong@gmail.com>
2022-02-28 09:35:13 +0800

    完成Chart7 环形图

--- src/components/Chart_7.tsx
+++ src/components/Chart_7.tsx
@@ -1,15 +1,65 @@
 import React, { useEffect, useRef } from 'react'
 import * as echarts from 'echarts'
+import { createEchartsOptions } from '../shared/create-echart-options'
+import { px } from '../shared/px'
 
 export const Chart7 = () => {
   const divRef = useRef(null)
+
   useEffect(() => {
+    console.log(divRef.current)
     var myChart = echarts.init(divRef.current)
+    myChart.setOption(
+      createEchartsOptions({
+        color: ['#8D70F8', '#33A4FA'],
+        xAxis: { show: false },
+        yAxis: { show: false },
+        legend: {
+          bottom: px(10),
+          itemWidth: px(20),
+          itemHeight: px(10),
+          itemGap: px(20),
+          textStyle: {
+            fontSize: px(16),
+            lineHeight: px(16),
+            color: '#a1d1f1',
+          },
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['55%', '65%'],
+            avoidLabelOverlap: false,
+            label: {
+              show: true,
+              position: 'inside',
+              textStyle: { color: '#a1d1f1', fontSize: px(20) },
+              formatter(options) {
+                return options.value * 100 + '%'
+              },
+            },
+            labelLine: { show: false },
+            itemStyle: {
+              borderColor: '#0F113A',
+              borderWidth: px(4),
+            },
+            data: [
+              { value: 0.2, name: '女' },
+              { value: 0.8, name: '男' },
+            ],
+          },
+        ],
+      })
+    )
   }, [])
 
   return (
     <div className="年龄段-图1">
-      <div ref={divRef} className="chart"></div>
+      <div className="chart">
+        <div className="main" ref={divRef} />
+        <div className="text">性别</div>
+      </div>
     </div>
   )
 }
