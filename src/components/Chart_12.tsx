47adc1d811266cb4eb7f8e24e22743e6e3b600e4
TravisWongX <travis.x.wong@gmail.com>
2022-03-01 09:04:51 +0800

    完成Chart12 饼图

--- src/components/Chart_12.tsx
+++ src/components/Chart_12.tsx
@@ -5,54 +5,67 @@ import { px } from '../shared/px'
 
 export const Chart12 = () => {
   const divRef = useRef(null)
-  
+
   useEffect(() => {
     const data = [
       { value: 0.08, name: '东岗路' },
       { value: 0.06, name: '段家滩' },
-      {value: 0.26, name: '雁北'},
-      {value: 0.09, name: '五泉山'},
-      {value: 0.15, name: '中山路'},
-      {value: 0.06, name: '庆阳路'},
-      {value: 0.08, name: '武都路'},
-      {value: 0.18, name: '酒泉路'},
-      {value: 0.04, name: '天水路'}
+      { value: 0.26, name: '雁北' },
+      { value: 0.09, name: '五泉山' },
+      { value: 0.15, name: '中山路' },
+      { value: 0.06, name: '庆阳路' },
+      { value: 0.08, name: '武都路' },
+      { value: 0.19, name: '酒泉路' },
+      { value: 0.03, name: '天水路' },
     ]
 
     var myChart = echarts.init(divRef.current)
-    const color = ['#8d70f8', '#f46064', '#f38e1c', '#1adc7d','#e8e800','#34a3f9','#40b27d','#ea7ccc','#ff33ef']
+    const color = [
+      '#8d70f8',
+      '#f46064',
+      '#f38e1c',
+      '#1adc7d',
+      '#e8e800',
+      '#34a3f9',
+      '#40b27d',
+      '#ea7ccc',
+      '#ff33ef',
+    ]
     myChart.setOption(
       createEchartsOptions({
         color: color,
         xAxis: { show: false },
         yAxis: { show: false },
-        grid: {x: 0, x2: 0, y: 0, y2: 0, containLabel: true},
+        grid: { x: 0, x2: 0, y: 0, y2: 0, containLabel: true },
         legend: {
           orient: 'vertical',
-          left: 0,
+          left: px(20),
           top: 'center',
+          icon: 'line',
           itemWidth: px(10),
-          itemHeight: px(10),
+          // itemHeight: px(10),
+          itemGap: px(10),
           textStyle: {
-            fontSize: px(16),
-            lineHeight: px(16),
-            color: '#a1d1f1',
+            fontSize: px(12),
+            lineHeight: px(12),
+            color: '#6ca8ed',
           },
           formatter(name) {
-            const value = data.filter(i => i.name === name)[0]?.value * 100 + '%';
-            return name + ' ' + value;
-          }
+            const value =
+              data.filter((i) => i.name === name)[0]?.value * 100 + '%'
+            return name + ' ' + value
+          },
         },
         series: [
           {
             name: '访问来源',
             type: 'pie',
-            radius: '60%',
+            radius: '75%',
             // 图例位置
-            center: ['55%', '50%'],
+            center: ['60%', '50%'],
             avoidLabelOverlap: false,
-            labelLine: { 
-              show: false,         
+            labelLine: {
+              show: false,
             },
             label: {
               show: false,
@@ -66,7 +79,5 @@ export const Chart12 = () => {
     )
   }, [])
 
-  return (
-        <div className="chart" ref={divRef} />
-  )
+  return <div className="chart" ref={divRef} />
 }
