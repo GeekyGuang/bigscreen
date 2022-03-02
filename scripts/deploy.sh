2d9bc1c07ff4ce93ab13026dfb070c570b7e3653
TravisWongX <travis.x.wong@gmail.com>
2022-03-02 17:33:15 +0800

    更新部署脚本

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,6 @@ git add . &&
 git commit -m deploy &&
 git remote add gitee git@gitee.com:XG_GX/big-screen-web.git &&
 git push -f gitee master &&
+git remote add github git@github.com:TravisWongX/big-screen-web.git &&
+git push -f github master &&
 cd -;
